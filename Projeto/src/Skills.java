3963332052e07113445bea0c912e7d3949368e72
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2024-10-22 21:47:00 -0300

    Criando a class Skills

---
+++ Projeto/src/Skills.java
@@ -0,0 +1,9 @@
+public abstract class Skills {
+    private String nomeAtaque;
+    private double dano;
+
+    public Skills(String nomeAtaque, double dano) {
+        this.nomeAtaque = nomeAtaque;
+        this.dano = dano;
+    }
+}
