a24ee4ea06068ff38d9ee01ecdc0efcb6d68be10
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2024-10-23 21:50:07 -0300

    Interface Gráfica de troca de pokemon

---
+++ Projeto/src/TrocarPokemon.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class TrocarPokemon {
+    private JButton selecionarButton2;
+    private JButton selecionarButton4;
+    private JButton selecionarButton3;
+    private JButton selecionarButton1;
+    private JButton selecionarButton;
+}
