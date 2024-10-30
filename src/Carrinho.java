de15fdbb0635031f9f6436e8f41b990600b55276
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2024-10-29 21:09:31 -0300

    Criando a classe Carrinho

---
+++ src/Carrinho.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Carrinho {
+    private ArrayList<Produto> carrinho = new ArrayList<>();
+
+    public void adicionarProduto(Produto produto) {
+        carrinho.add(produto);
+    }
+
+    public void deletaItemCarrinho(Produto produto) {
+        carrinho.remove(produto);
+    }
+
+
+
+}
