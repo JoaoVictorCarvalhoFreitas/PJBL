d6cee2f46dcf3b6bc9607f6067e0aaa6d81cba87
FZhuu <felipezhangzhu@gmail.com>
2024-10-30 22:11:42 -0300

    Interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Login login = new Login();
+        Frame fm = new Frame();
+        fm.add(login.criarPainelLogin());
+        fm.setVisible(true);
     }
 }
