2208f77ba311b21c545a3c5c809e3e4817fe5d03
FZhuu <felipezhangzhu@gmail.com>
2024-10-30 22:13:48 -0300

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 9bb34a35a584b148e4a45ad30502c681d953a0b2, reversing
    changes made to d6cee2f46dcf3b6bc9607f6067e0aaa6d81cba87.

--- src/Dados.java
+++ src/Dados.java
@@ -3,144 +3,138 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public abstract class Dados {
-    static String arquivoClientes = "clientes.ser";
-    static String arquivoAdmin = "Adms.ser";
-    static String arquivoUsuario = "Usuarios.ser";
-    static String arquivoProdutos = "produtos.ser";
+    private static String  arquivoClientes = "clientes.ser" ;
+    private static String arquivoAdmin = "Adms.ser";
+    private static String arquivoProdutos = "produtos.ser";
 
 
-
-
-    public static Cliente logar(String email, String senha) {
+    // Exemplo de método de login que retorna o usuário
+    public static Usuario logar(String email, String senha) {
         Cliente cliente = obterClientePorEmail(email);
         if (cliente == null) {
             return null;
         }
-        if (LogarUsuario(email, senha)) { // Ajuste para sua lógica de senha
+        if (LogarUsuario(email,senha)) { // Ajuste para sua lógica de senha
             return cliente;
         }
         return null;
     }
 
+
     //ADMS
-//    public static void salvarAdm(String nome, String email, String senha) {
-//        Admin adm = new Admin(obterAdms().size() + 1, nome, email, senha);
-//        try (FileOutputStream fileOut = new FileOutputStream(arquivoAdmin, true);
-//             ObjectOutputStream out = new ObjectOutputStream(fileOut) {
-//                 protected void writeStreamHeader() throws IOException {
-//                     if (fileOut.getChannel().position() == 0) {
-//                         super.writeStreamHeader();
-//                     } else {
-//                         reset();
-//                     }
-//                 }
-//             }) {
-//            out.writeObject(adm);
-//            System.out.println("adm salvo com sucesso: " + adm.getNome());
-//        } catch (IOException e) {
-//            System.out.println("Erro ao salvar o adm: " + e.getMessage());
-//        }
-//    }
-//
-//    private static ArrayList<Admin> obterAdms() {
-//        ArrayList<Admin> adms = new ArrayList<>();
-//        try (FileInputStream fileIn = new FileInputStream(arquivoAdmin);
-//             ObjectInputStream in = new ObjectInputStream(fileIn)) {
-//            while (true) {
-//                try {
-//                    Admin adm = (Admin) in.readObject();
-//                    adms.add(adm);
-//                } catch (EOFException e) {
-//                    break;
-//                }
-//            }
-//        } catch (IOException | ClassNotFoundException e) {
-//            System.out.println("Erro ao carregar adms: " + e.getMessage());
-//        }
-//        return adms;
-//    }
-//
-//    private static boolean logarAdm(String email, String senha) {
-//        Admin adm = obterAdminPorEmail(email);
-//        if (adm == null) {
-//            return false;
-//        }
-//        if (adm.getSenha().equals(senha)) return true;
-//        return false;
-//
-//    }
-//
-//    public static Admin logaAdm(String email, String senha) {
-//        Admin adm = obterAdminPorEmail(email);
-//
-//        if (adm == null) {
-//            return null;
-//        }
-//        if (adm.getSenha().equals(senha)) return adm;
-//        return null;
-//    }
-//
-//    private static Admin obterAdminPorEmail(String email) {
-//        ArrayList<Admin> adms = obterAdms();
-//        for (Admin adm : adms) {
-//            if (adm.getEmail().equals(email)) {
-//                return adm;
-//            }
-//        }
-//        return null;
-//    }
+    public static void salvarAdm(String nome,String email,String senha){
+        Admin adm = new Admin(obterAdms().size()+1,nome,email,senha);
+        try (FileOutputStream fileOut = new FileOutputStream(arquivoAdmin, true);
+             ObjectOutputStream out = new ObjectOutputStream(fileOut) {
+                 protected void writeStreamHeader() throws IOException {
+                     if (fileOut.getChannel().position() == 0) {
+                         super.writeStreamHeader();
+                     } else {
+                         reset();
+                     }
+                 }
+             }) {
+            out.writeObject(adm);
+            System.out.println("adm salvo com sucesso: " + adm.getNome());
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar o adm: " + e.getMessage());
+        }
+    }
+
+    private static ArrayList<Admin> obterAdms() {
+        ArrayList<Admin> adms = new ArrayList<>();
+        try (FileInputStream fileIn = new FileInputStream(arquivoAdmin);
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            while (true) {
+                try {
+                    Admin adm = (Admin) in.readObject();
+                    adms.add(adm);
+                } catch (EOFException e) {
+                    break;
+                }
+            }
+        }  catch (IOException | ClassNotFoundException e) {
+            System.out.println("Erro ao carregar adms: " + e.getMessage());
+        }
+        return adms;
+    }
+
+    public static boolean logarAdm(String email, String senha){
+        Admin adm = obterAdminPorEmail(email);
+        if(adm == null){
+            return false;
+        }
+        if (adm.getSenha().equals(senha)) return true;
+        return false;
+
+    }
+
+    public static Admin logaAdm(String email,String senha){
+        Admin adm = obterAdminPorEmail(email);
+
+        if(adm == null){
+            return null;
+        }
+        if (adm.getSenha().equals(senha)) return adm;
+        return null;
+    }
+
+    private static Admin obterAdminPorEmail(String email){
+        ArrayList<Admin> adms = obterAdms();
+        for (Admin adm : adms) {
+            if (adm.getEmail().equals(email)) {
+                return adm;
+            }
+        }
+        return null;
+    }
 
     // Clientes
-    public static boolean LogarUsuario(String email, String senha) {
+    public static boolean LogarUsuario(String email, String senha){
         Cliente c = obterClientePorEmail(email);
-        if (c == null) {
+        if(c == null){
             return false;
         }
         if (c.getSenha().equals(senha)) return true;
         return false;
     }
 
-    public static void salvarCliente(Cliente cliente) {
-
+    private static Cliente obterClientePorEmail(String email){
         ArrayList<Cliente> clientes = obterclientes();
-
-
-        boolean clienteExiste = false;
-        for (Cliente cl : clientes) {
-            if (cl.getNome().equals(cliente.getNome())) {
-                clienteExiste = true;
-                break;
+        for (Cliente cliente : clientes) {
+            if (cliente.getEmail().equals(email)) {
+                return cliente;
             }
         }
+        return null;
+    }
 
-        if (!clienteExiste) {
-            clientes.add(cliente);
-            try (FileOutputStream fileOut = new FileOutputStream(arquivoClientes);
-                 ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
-                for (Cliente cliente1 : clientes) {
-                    out.writeObject(cliente1);
-                }
-                System.out.println("Cliente salvo com sucesso: " + cliente.getNome());
-            } catch (IOException e) {
-                System.out.println("Erro ao salvar o cliente: " + e.getMessage());
-            }
-        } else {
-            System.out.println("cliente já existe: " + cliente.getNome());
+    public static void salvarCliente(Cliente cliente) {
+        boolean arquivoExiste = new File(arquivoClientes).exists();
+
+        try (FileOutputStream fileOut = new FileOutputStream(arquivoClientes, true);
+             ObjectOutputStream out = arquivoExiste ? new ObjectOutputStream(fileOut) {
+                 protected void writeStreamHeader() throws IOException {
+                     reset(); // Evita regravar o cabeçalho
+                 }
+             }
+                     : new ObjectOutputStream(fileOut)) { // Escreve o cabeçalho se o arquivo é novo
+            out.writeObject(cliente);
+            System.out.println("Cliente salvo com sucesso: " + cliente.getNome());
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar o cliente: " + e.getMessage());
         }
     }
 
-    public static void salvarCliente(String nome, String email, String senha) {
-        Cliente cliente = new Cliente(obterclientes().size() + 1, nome, email, senha);
+    public static void salvarCliente(String nome,String email,String senha) {
+        Cliente cliente = new Cliente(obterclientes().size()+1,nome,email,senha);
         salvarCliente(cliente);
     }
 
     private static void salvarclientes(ArrayList<Cliente> clientes) {
         try (FileOutputStream fileOut = new FileOutputStream(arquivoClientes);
              ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
-            for (Cliente c : clientes) {
-                out.writeObject(c);
-
-            }
+            out.writeObject(clientes);
             System.out.println("clientes atualizados com sucesso.");
         } catch (IOException e) {
             System.out.println("Erro ao salvar clientes: " + e.getMessage());
@@ -151,16 +145,16 @@ public abstract class Dados {
         ArrayList<Cliente> clientes = new ArrayList<>();
         try (FileInputStream fileIn = new FileInputStream(arquivoClientes);
              ObjectInputStream in = new ObjectInputStream(fileIn)) {
-            while (true) {
+            while (true){
 
-                try {
+                try{
                     Cliente cliente = (Cliente) in.readObject();
                     clientes.add(cliente);
-                } catch (EOFException e) {
+                }catch (EOFException e) {
                     break;
                 }
             }
-        } catch (IOException | ClassNotFoundException e) {
+        }  catch (IOException | ClassNotFoundException e) {
             System.out.println("Erro ao carregar clientes: " + e.getMessage());
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -171,7 +165,7 @@ public abstract class Dados {
     public static Cliente obterclientePorId(int Id) {
         ArrayList<Cliente> clientes = obterclientes();
         for (Cliente cliente : clientes) {
-            if (cliente.getId() == Id) {
+            if (cliente.getId()==Id) {
                 return cliente;
             }
         }
@@ -192,9 +186,9 @@ public abstract class Dados {
     public static void deletarcliente(int Id) {
         ArrayList<Cliente> clientes = obterclientes();
         for (int i = 0; i < clientes.size(); i++) {
-            if (clientes.get(i).getId() == Id) {
+            if (clientes.get(i).getId()==Id) {
                 clientes.remove(i);
-                System.out.println("Cliente: " + clientes.get(i).getNome() + "removido com sucesso.");
+                System.out.println("Cliente: " +clientes.get(i).getNome() + "removido com sucesso.");
             }
         }
         salvarclientes(clientes);
@@ -229,6 +223,7 @@ public abstract class Dados {
         }
     }
 
+
     private static void salvarProdutos(ArrayList<Produto> produtos) {
         try (FileOutputStream fileOut = new FileOutputStream("produtos.ser");
              ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
@@ -241,9 +236,9 @@ public abstract class Dados {
         }
     }
 
-    public static void salvarProduto(String nome, String descricao, double preco) {
+    public static void salvarProduto(String nome,String descricao,double preco){
         int id = Dados.obterProdutos().size();
-        salvarProduto(new Produto(id, nome, descricao, preco));
+        salvarProduto(new Produto(id,nome,descricao,preco));
     }
 
     public static ArrayList<Produto> obterProdutos() {
@@ -254,12 +249,12 @@ public abstract class Dados {
                 try {
                     Produto produto = (Produto) in.readObject();
                     produtos.add(produto);
-                } catch (EOFException e) {
+                }catch (EOFException e) {
                     break;
                 }
             }
 
-        } catch (IOException | ClassNotFoundException e) {
+        }catch (IOException | ClassNotFoundException e) {
             System.out.println("Erro ao carregar produtos: " + e.getMessage());
         }
         return produtos;
@@ -298,7 +293,7 @@ public abstract class Dados {
         return null;
     }
 
-    public static void atualizarProduto(String nome, String descricao, double preco) {
+    public static void atualizarProduto(String nome,String descricao,double preco) {
         ArrayList<Produto> produtos = obterProdutos();
         boolean produtoEncontrado = false;
 
@@ -319,7 +314,7 @@ public abstract class Dados {
         }
     }
 
-    public static void atualizaPrecoProduto(String nome, double preco) {
+    public static void atualizaPrecoProduto(String nome,double preco){
         ArrayList<Produto> produtos = obterProdutos();
         for (int i = 0; i < produtos.size(); i++) {
             if (produtos.get(i).getNome().equals(nome)) {
@@ -341,150 +336,17 @@ public abstract class Dados {
     }
 
 
-    //    CARRINHO
-    public static Carrinho getCarrinho(int id) {
+//    CARRINHO
+    public static Carrinho getCarrinho(int id){
         Cliente c = Dados.obterclientePorId(id);
         return c.getCarrinho();
     }
 
-    public static void DeletarItemCarrinho(int idCliente, int idProduto) {
+    public static void DeletarItemCarrinho(int idCliente,int idProduto){
         Cliente c = Dados.obterclientePorId(idCliente);
         c.getCarrinho().deletaItemCarrinho(Dados.obterProdutoPorId(idProduto));
-        atualizarcliente(idCliente, c);
+        atualizarcliente(idCliente,c);
 
-    }
-
-
-
-    //Usuarios
-
-
-    public static ArrayList<Cliente> obterUsuariosCliente() {
-        ArrayList<Cliente> clientes = new ArrayList<>();
-
-        try (FileInputStream fileIn = new FileInputStream(arquivoUsuario);
-             ObjectInputStream in = new ObjectInputStream(fileIn)) {
-
-            while (true) {
-                try {
-                    if(in.readObject() instanceof Cliente u){
-                        clientes.add(u);
-                    }
-
-
-                } catch (EOFException e) {
-                    break;
-                }
-            }
-
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Erro ao carregar clientes: " + e.getMessage());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return clientes;
-
-    }
-
-    public static ArrayList<Admin> obterUsuariosAdmins() {
-        ArrayList<Admin> admins = new ArrayList<>();
-
-        try (FileInputStream fileIn = new FileInputStream(arquivoUsuario);
-             ObjectInputStream in = new ObjectInputStream(fileIn)) {
-
-            while (true) {
-                try {
-                    if(in.readObject() instanceof Admin u){
-                        admins.add(u);
-                    }
-
-
-                } catch (EOFException e) {
-                    break;
-                }
-            }
-
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Erro ao carregar clientes: " + e.getMessage());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return admins;
-
-    }
-
-    public static Cliente obterUsuarioClientePorEmail(String email) {
-        ArrayList<Cliente>Clientes = obterUsuariosCliente();
-        for (Cliente c : Clientes) {
-            if (c.getEmail().equals(email)) {
-                return c;
-            }
-        }
-        return null;
-    }
-
-    private static void cadastraUsuarioCliente(Cliente usuario){
-
-        ArrayList<Cliente> usuarios = obterUsuariosCliente();
-        int id = usuarios.size()+1;
-
-        boolean clienteExiste = false;
-        for (Usuario u: usuarios) {
-            if (u.getNome().equals(usuario.getNome())) {
-                clienteExiste = true;
-                break;
-            }
-        }
-
-        if (!clienteExiste) {
-            usuarios.add(usuario);
-            try (FileOutputStream fileOut = new FileOutputStream(arquivoClientes);
-                 ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
-                for (Usuario u1 : usuarios) {
-                    out.writeObject(u1); // Grava todos os produtos no arquivo
-                }
-                System.out.println("Cliente salvo com sucesso: " + usuario.getNome());
-            } catch (IOException e) {
-                System.out.println("Erro ao salvar o cliente: " + e.getMessage());
-            }
-        } else {
-            System.out.println("cliente já existe: " + usuario.getNome());
-        }
-
-
-    }
-
-    public static void cadastraCliente(String email,String nome,String senha){
-
-        int id = obterUsuariosCliente().size()+1;
-        cadastraUsuarioCliente(new Cliente(id,nome,email,senha));
-
-    }
-
-    public static void salvaUsuarioCliente(int id,Cliente cliente){
-        ArrayList<Cliente> clientes = obterUsuariosCliente();
-        for (int i = 0; i < clientes.size(); i++) {
-            if (clientes.get(i).getId() == id) {
-                clientes.set(i, cliente);
-                break;
-            }
-        }
-
-    }
-    public static void salvaUsuarioClientes(ArrayList<Cliente> clientes){
-        ArrayList<Admin> admins = obterUsuariosAdmins();
-
-        try (FileOutputStream fileOut = new FileOutputStream(arquivoClientes);
-             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
-            for (Cliente c : clientes) {
-                out.writeObject(c);
-
-            }
-            System.out.println("clientes atualizados com sucesso.");
-        } catch (IOException e) {
-            System.out.println("Erro ao salvar clientes: " + e.getMessage());
-        }
     }
 
 }
-
