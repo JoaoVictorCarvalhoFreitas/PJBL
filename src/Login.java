a0b86fcc3ce6359cf5a09117f84c3aefba6bdff2
FZhuu <felipezhangzhu@gmail.com>
2024-10-30 22:13:44 -0300

    Revert "Interface"
    This reverts commit d6cee2f46dcf3b6bc9607f6067e0aaa6d81cba87.

--- src/Login.java
+++ src/Login.java
@@ -54,7 +54,7 @@ public class Login {
                 if (String.valueOf(senha.getPassword()).equals(placeholdersenha)) {
                     senha.setText("");
                     senha.setForeground(Color.BLACK);
-                    senha.setEchoChar('•');
+                    senha.setEchoChar('*');
 
                 }
             }
